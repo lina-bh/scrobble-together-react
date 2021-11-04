4e7651e82735314d406cb79adc628404bb7fecda
Lina <36717206+linabeee@users.noreply.github.com>
2021-11-04 12:08:04 +0000

    make reference to web vitals. but don't use it

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
-import React from "react"
+import { StrictMode } from "react"
 import ReactDOM from "react-dom"
 import "bootstrap/dist/css/bootstrap.min.css"
+// import reportWebVitals from "./reportWebVitals"
 
 import "./index.css"
 import App from "./App"
 import { AuthProvider } from "./AuthContext"
 
 ReactDOM.render(
-  <React.StrictMode>
+  <StrictMode>
     <AuthProvider>
       <App />
     </AuthProvider>
-  </React.StrictMode>,
+  </StrictMode>,
   document.getElementById("root")
 )
+
+// reportWebVitals(console.log)
